c45b36a58e87b74108359cd628101610d1610f56
karthik0932 <54766059+karthik0932@users.noreply.github.com>
2020-05-21 21:22:03 +0530

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,25 @@
+n = int(input("enter number \n"))
+for i in range(0, n + 1):
+    for j in range(n - i, 0, -1):
+        print(j, end=" ")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      Question  :--
+      enter number 5
+      5 4 3 2 1 
+      4 3 2 1 
+      3 2 1 
+      2 1
+      1
